96e86571a9cab5ce0adb123cccffeddd3054995a
Lestcreator <87945574+Lestcreator@users.noreply.github.com>
2022-06-28 11:59:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const chalk = require("chalk")
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
@@ -49,7 +49,7 @@ global.location = "jatim" //ur location
 
 //bot bombdy
 global.botname = "lestky" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.websitex = "https://vm.tiktok.com/ZSdw3tmef/?k=1" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
 global.author = "🦄Dream Guy Xeon"
